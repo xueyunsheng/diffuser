fa623453122b9d3c10970c424ec307fb1ae84906
xueyunsheng <109709374+xueyunsheng@users.noreply.github.com>
2024-01-23 20:10:33 +0800

    diffusion.py

--- diffuser/models/diffusion.py
+++ diffuser/models/diffusion.py
@@ -19,8 +19,9 @@ Sample = namedtuple('Sample', 'trajectories values chains')
 @torch.no_grad()
 def default_sample_fn(model, x, cond, t):
     model_mean, _, model_log_variance = model.p_mean_variance(x=x, cond=cond, t=t)
+    #根据输入、条件和步数 生成均值和方差
     model_std = torch.exp(0.5 * model_log_variance)
-
+    #计算标准差
     # no noise when t == 0
     noise = torch.randn_like(x)
     noise[t == 0] = 0
